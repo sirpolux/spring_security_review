6c6c5ef4fc75af3c81b5580020929d0efd9e683d
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-09-03 20:10:44 +0100

    update

--- src/main/java/com/study/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/study/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,20 @@
 package com.study.springsecurity.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig
 {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf(customizer->customizer.disable());
+        httpSecurity.authorizeHttpRequests(requests-> requests.anyRequest().authenticated());
+        return httpSecurity.build();
+    }
 }
